8439bc35dcc2ac2c03eac78fb209732810a1c7a8
Puck-The-Bard <zdmcguire1@buffs.wtamu.edu>
2019-09-04 22:37:21 -0500

    Finished name jen

---
+++ HW1_ZM.js
@@ -0,0 +1,26 @@
+//console test
+console.log('Wazzup');
+
+function RandomName(Name){
+
+console.log(name);
+
+var RandNames = [" The Drunk", " The Incontenant", " The Spud", " The Less than Resiliant", " The Onion Eyed Oaf", " The Lewd"];
+
+let random = Math.floor(Math.random() * (5 - 0 + 1) ) + 0;
+
+console.log(Name + RandNames[random]);
+
+return Name + RandNames[random];
+}
+
+var name = prompt("please enter your name");
+
+document.getElementById("nameGen").innerHTML = "Your Nickname is: " + RandomName(name);
+
+//Links to resources used
+/*
+https://www.youtube.com/watch?v=W6NZfCO5SIk - Mainly this
+
+https://www.w3schools.com/js/js_arrays.asp
+*/
